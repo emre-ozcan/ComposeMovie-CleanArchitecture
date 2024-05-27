1a4b9001cd9e829f603e0bf38894c21a9fea0c9e
Emre Özcan <eozcan97@gmail.com>
2024-05-27 21:01:54 +0300

    Dependencies Added

--- app/src/main/java/com/emreozcan/composemovie/MainActivity.kt
+++ app/src/main/java/com/emreozcan/composemovie/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeMovieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
